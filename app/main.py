be41e6872cf6a3693bc90f77ec98f20d6cecb7db
ilevk <kcs93023@gmail.com>
2024-04-29 15:31:28 +0900

    refactor - config 수정

--- app/main.py
+++ app/main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 
 from app.core.lifespan import lifespan
 from app.routers import router
+from app.core.config import config
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, **config.fastapi_kwargs)
 
 app.include_router(router)
 

0ee63cf5db5eb6f21d3be94d2ce2398101ae3018
ilevk <kcs93023@gmail.com>
2024-04-29 22:28:19 +0900

    feat - add redis cache

--- app/core/logger.py
+++ app/core/logger.py
@@ -3,7 +3,7 @@ import logging
 from app.core.config import config
 
 
-def init_logger() -> None:
+def init_logger() -> logging.Logger:
     logger = logging.getLogger("uvicorn")
     logger.setLevel(config.LOG_LEVEL)
 

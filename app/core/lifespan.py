0268cca1034db97a8c5f7a74a61aa4f01caeb1ca
ilevk <kcs93023@gmail.com>
2024-04-29 22:28:19 +0900

    feat - add redis cache

--- app/core/lifespan.py
+++ app/core/lifespan.py
@@ -1,13 +1,16 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
+from app.core.redis import redis_cache
 from app.core.db.session import ping_db, close_db
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await ping_db()
+    await redis_cache.ping()
 
     yield
 
     await close_db()
+    await redis_cache.close()

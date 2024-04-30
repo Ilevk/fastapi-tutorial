9784577fed14f411299f5d7fb62c50291d1fe1de
kimjae-1 <kimjae1@g.skku.edu>
2024-04-30 03:57:39 +0000

    add lifespan

---
+++ app/core/lifespan.py
@@ -0,0 +1,13 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+from app.core.db.session import ping_db, close_db
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await ping_db()
+
+    yield
+
+    await close_db()

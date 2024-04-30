289efe384735a2367056527ba5f0c84531c7c129
kimjae-1 <kimjae1@g.skku.edu>
2024-04-30 04:10:08 +0000

    cors

--- app/core/config.py
+++ app/core/config.py
@@ -16,7 +16,7 @@ class Config(BaseSettings):
 
     LOG_LEVEL: int = logging.DEBUG
 
-    DB_URL: str = "postgresql+asyncpg://postgres:password@localhost:5432/postgres"
+    DB_URL: str = "postgresql+asyncpg://postgres:ls_mle_course@34.22.81.66:5432/academy"
 
     REDIS_HOST: str = "localhost"
     REDIS_PORT: int = 6379
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.lifespan import lifespan
 from app.routers import router
@@ -7,6 +8,13 @@ from app.core.config import config
 app = FastAPI(lifespan=lifespan, **config.fastapi_kwargs)
 
 app.include_router(router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")

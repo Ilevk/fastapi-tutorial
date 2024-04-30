7b2680b99626b8205e5d5356b4228bb2833ad4c1
ilevk <kcs93023@gmail.com>
2024-04-30 12:24:21 +0900

    feat - add database engine, session

--- app/core/db/session.py
+++ app/core/db/session.py
@@ -1,4 +1,35 @@
+from asyncio import current_task
+
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_scoped_session,
+    async_sessionmaker,
+    create_async_engine,
+)
 from sqlalchemy.orm import DeclarativeBase
+
+from app.core.config import config, is_local
 
 
 class Base(DeclarativeBase): ...
+
+
+engine = create_async_engine(
+    config.DB_URL, pool_size=10, max_overflow=5, echo=is_local(), pool_pre_ping=True
+)
+
+async_session_factory = async_sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+AsyncScopedSession = async_scoped_session(async_session_factory, scopefunc=current_task)
+
+
+async def ping_db():
+    async with engine.begin() as conn:
+        await conn.execute(text("SELECT 1"))
+
+
+async def close_db():
+    await engine.dispose()

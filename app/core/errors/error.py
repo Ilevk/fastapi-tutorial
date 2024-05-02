96128c4e3f3fd986be456e7323cfad8621325d73
ilevk <kcs93023@gmail.com>
2024-05-02 12:25:51 +0900

    feat - add error handling

--- app/core/errors/error.py
+++ app/core/errors/error.py
@@ -5,8 +5,16 @@ ERROR_400_CLASS_NOTICE_CREATION_FAILED = "40003"
 ERROR_400_CLASS_NOTICE_UPDATE_FAILED = "40004"
 ERROR_400_CLASS_NOTICE_DELETE_FAILED = "40005"
 
+ERROR_401_INVALID_API_KEY = "40100"
+
 
 class BaseAPIException(Exception):
+    def __init__(self, code: str, message: str):
+        self.code = code
+        self.message = message
+
+
+class BaseAuthException(Exception):
     def __init__(self, code: str, message: str):
         self.code = code
         self.message = message
@@ -53,3 +61,8 @@ class ClassNoticeDeleteFailed(BaseAPIException):
             code=ERROR_400_CLASS_NOTICE_DELETE_FAILED,
             message="Class Notice delete failed",
         )
+
+
+class InvalidAPIKey(BaseAuthException):
+    def __init__(self):
+        super().__init__(code=ERROR_401_INVALID_API_KEY, message="Invalid API Key")
--- app/main.py
+++ app/main.py
@@ -5,14 +5,15 @@ from starlette_context.middleware import ContextMiddleware
 from app.core.config import config
 from app.core.lifespan import lifespan
 from app.core.middlewares.sqlalchemy import SQLAlchemyMiddleware
-from app.core.errors.error import BaseAPIException
-from app.core.errors.handler import api_error_handler
+from app.core.errors.error import BaseAPIException, BaseAuthException
+from app.core.errors.handler import api_error_handler, api_auth_error_handler
 from app.routers import router
 
 app = FastAPI(lifespan=lifespan, **config.fastapi_kwargs)
 
 app.include_router(router)
 app.add_exception_handler(BaseAPIException, api_error_handler)
+app.add_exception_handler(BaseAuthException, api_auth_error_handler)
 
 app.add_middleware(
     CORSMiddleware,

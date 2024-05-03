a6896fe9247d04ad916447bba0d40b916b16cc1a
kimjae-1 <kimjae1@g.skku.edu>
2024-05-03 09:09:15 +0000

    feat - add error handling

--- app/core/errors/error.py
+++ app/core/errors/error.py
@@ -5,12 +5,17 @@ ERROR_400_CLASS_NOTICE_CREATION_FAILED = "40003"
 ERROR_400_CLASS_NOTICE_UPDATE_FAILED = "40004"
 ERROR_400_CLASS_NOTICE_DELETE_FAILED = "40005"
 
+ERROR_401_INVALID_API_KEY = "40100"
 
 class BaseAPIException(Exception):
     def __init__(self, code: str, message: str):
         self.code = code
         self.message = message
 
+class BaseAuthException(Exception):
+    def __init__(self, code: str, message: str):
+        self.code = code
+        self.message = message
 
 class ClassNotFoundException(BaseAPIException):
     def __init__(self):
@@ -53,3 +58,7 @@ class ClassNoticeDeleteFailed(BaseAPIException):
             code=ERROR_400_CLASS_NOTICE_DELETE_FAILED,
             message="Class Notice delete failed",
         )
+
+class InvalidAPIKey(BaseAuthException):
+    def __init__(self):
+        super().__init__(code=ERROR_401_INVALID_API_KEY, message="Invalid API Key")

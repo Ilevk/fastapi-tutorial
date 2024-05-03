01ceaf61749e890a60fdf065b3cf0ddc4a5c7af2
ilevk <kcs93023@gmail.com>
2024-05-03 18:58:01 +0900

    feat - apply dependency injector

--- app/main.py
+++ app/main.py
@@ -4,26 +4,37 @@ from starlette_context.middleware import ContextMiddleware
 
 from app.core.config import config
 from app.core.lifespan import lifespan
+from app.core.container import Container
 from app.core.middlewares.sqlalchemy import SQLAlchemyMiddleware
 from app.core.errors.error import BaseAPIException, BaseAuthExeption
 from app.core.errors.handler import api_error_handler, api_auth_error_handler
 from app.routers import router
 
-app = FastAPI(lifespan=lifespan, **config.fastapi_kwargs)
 
-app.include_router(router)
-app.add_exception_handler(BaseAPIException, api_error_handler)
-app.add_exception_handler(BaseAuthExeption, api_auth_error_handler)
+def create_app() -> FastAPI:
+    app = FastAPI(lifespan=lifespan, **config.fastapi_kwargs)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-app.add_middleware(SQLAlchemyMiddleware)
-app.add_middleware(ContextMiddleware)
+    container = Container()
+    container.config.from_dict(config.dict())
+
+    app.include_router(router)
+    app.add_exception_handler(BaseAPIException, api_error_handler)
+    app.add_exception_handler(BaseAuthExeption, api_auth_error_handler)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    app.add_middleware(SQLAlchemyMiddleware)
+    app.add_middleware(ContextMiddleware)
+
+    return app
+
+
+app = create_app()
 
 
 @app.get("/")

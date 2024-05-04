f77a43b08508539f90a65488082da47c7cf590d0
ilevk <kcs93023@gmail.com>
2024-05-04 11:31:58 +0900

    refactor - add redis cache decorator

--- app/core/redis.py
+++ app/core/redis.py
@@ -1,9 +1,11 @@
 import pickle
 from typing import Optional
+from functools import wraps
 
 from redis.asyncio import Redis
 
 from app.core.config import config
+from app.core.logger import logger
 
 
 class RedisCache:
@@ -30,8 +32,30 @@ class RedisCache:
         return await self.redis.exists(key)
 
 
-def key_builder(*args) -> str:
-    return ":".join(map(str, args))
+class RedisCacheDecorator:
+    def __init__(self, ttl: int = 60):
+        self.ttl = ttl
+
+    def key_builder(self, *args) -> str:
+        return ":".join(map(str, args))
+
+    def __call__(self, func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            _key = self.key_builder(func.__name__, *args, *kwargs)
+
+            if await redis_cache.exists(_key):
+                logger.debug("Cache hit")
+                result = await redis_cache.get(_key)
+            else:
+                logger.debug("Cache miss")
+                result = await func(*args, **kwargs)
+                if result:
+                    await redis_cache.set(_key, result, ttl=self.ttl)
+
+            return result
+
+        return wrapper
 
 
 redis_cache = RedisCache()

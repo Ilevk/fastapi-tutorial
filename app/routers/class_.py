acf35b449e538e872b2959efa5d4b5c745d5a592
ilevk <kcs93023@gmail.com>
2024-05-04 12:55:26 +0900

    feat - add inject class

--- app/routers/class_.py
+++ app/routers/class_.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Query, Path, Body
+from fastapi import APIRouter, Query, Path, Body, Depends
+from dependency_injector.wiring import Provide, inject
 
 from app import services
-from app import repositories
+from app.core.container import Container
 from app.models.schemas.common import BaseResponse, HttpResponse, ErrorResponse
 from app.models.schemas.class_ import (
     ClassReq,
@@ -20,8 +21,10 @@ router = APIRouter()
     response_model=BaseResponse[ClassResp],
     responses={400: {"model": ErrorResponse}},
 )
+@inject
 async def create_class(
     request_body: ClassReq = Body(..., description="Class creation request body"),
+    class_service: services.ClassService = Depends(Provide[Container.class_service]),
 ) -> BaseResponse[ClassResp]:
     """
     POST
@@ -33,7 +36,6 @@ async def create_class(
     Returns:
         BaseResponse[ClassResp]: 클래스 생성 결과
     """
-    class_service = services.ClassService(repositories.ClassRepository())
     result = await class_service.create_class(request_body.to_dto())
 
     return HttpResponse(content=ClassResp.from_dto(result))
@@ -44,11 +46,12 @@ async def create_class(
     response_model=BaseResponse[ClassListResp],
     responses={400: {"model": ErrorResponse}},
 )
+@inject
 async def read_class_list(
     page: int = Query(1, ge=1, description="Page number"),
     limit: int = Query(10, ge=10, le=100, description="Number of items per page"),
+    class_service: services.ClassService = Depends(Provide[Container.class_service]),
 ) -> BaseResponse[ClassListResp]:
-    class_service = services.ClassService(repositories.ClassRepository())
     result = await class_service.read_class_list(page, limit)
 
     return HttpResponse(content=ClassListResp.from_dto(result))
@@ -59,10 +62,11 @@ async def read_class_list(
     response_model=BaseResponse[ClassResp],
     responses={400: {"model": ErrorResponse}},
 )
+@inject
 async def read_class(
-    class_id: str = Path(..., description="Class ID")
+    class_id: str = Path(..., description="Class ID"),
+    class_service: services.ClassService = Depends(Provide[Container.class_service]),
 ) -> BaseResponse[ClassResp]:
-    class_service = services.ClassService(repositories.ClassRepository())
     result = await class_service.read_class(class_id)
 
     return HttpResponse(content=ClassResp.from_dto(result))
@@ -73,13 +77,14 @@ async def read_class(
     response_model=BaseResponse[ClassNoticeResp],
     responses={400: {"model": ErrorResponse}},
 )
+@inject
 async def create_class_notice(
     class_id: str = Path(..., description="Class ID"),
     request_body: ClassNoticeReq = Body(
         ..., description="Class notice creation request body"
     ),
+    class_service: services.ClassService = Depends(Provide[Container.class_service]),
 ) -> BaseResponse[ClassNoticeResp]:
-    class_service = services.ClassService(repositories.ClassRepository())
     result = await class_service.create_class_notice(request_body.to_dto(class_id))
 
     return HttpResponse(content=ClassNoticeResp.from_dto(result))
@@ -90,12 +95,13 @@ async def create_class_notice(
     response_model=BaseResponse[ClassNoticeListResp],
     responses={400: {"model": ErrorResponse}},
 )
+@inject
 async def read_class_notice_list(
     class_id: str = Path(..., description="Class ID"),
     page: int = Query(1, ge=1, description="Page number"),
     limit: int = Query(10, ge=10, le=100, description="Number of items per page"),
+    class_service: services.ClassService = Depends(Provide[Container.class_service]),
 ) -> BaseResponse[ClassNoticeListResp]:
-    class_service = services.ClassService(repositories.ClassRepository())
     result = await class_service.read_class_notice_list(class_id, page, limit)
 
     return HttpResponse(content=ClassNoticeListResp.from_dto(result))
@@ -106,14 +112,15 @@ async def read_class_notice_list(
     response_model=BaseResponse[ClassNoticeResp],
     responses={400: {"model": ErrorResponse}},
 )
+@inject
 async def update_class_notice(
     class_id: str = Path(..., description="Class ID"),
     notice_id: int = Path(..., description="Notice ID"),
     request_body: ClassNoticeReq = Body(
         ..., description="Class notice update request body"
     ),
+    class_service: services.ClassService = Depends(Provide[Container.class_service]),
 ) -> BaseResponse[ClassNoticeResp]:
-    class_service = services.ClassService(repositories.ClassRepository())
     result = await class_service.update_class_notice(
         request_body.to_dto(class_id, notice_id)
     )
@@ -126,11 +133,12 @@ async def update_class_notice(
     response_model=BaseResponse[ClassNoticeResp],
     responses={400: {"model": ErrorResponse}},
 )
+@inject
 async def delete_class_notice(
     class_id: str = Path(..., description="Class ID"),
     notice_id: int = Path(..., description="Notice ID"),
+    class_service: services.ClassService = Depends(Provide[Container.class_service]),
 ) -> BaseResponse[ClassNoticeResp]:
-    class_service = services.ClassService(repositories.ClassRepository())
     result = await class_service.delete_class_notice(class_id, notice_id)
 
     return HttpResponse(content=ClassNoticeResp.from_dto(result))
